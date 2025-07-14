3d67d1c047b2ce995b87f30f18878d7e65a35f44
Aung Chan Myae <myae0517@gmail.com>
2025-07-14 21:49:17 +0630

    Fix: hide PHP warnings and deprecated notices for production

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+// 🔇 Disable all error display for production (Render)
+error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', 0);
 /**
  * This file is part of the Cockpit project.
  *
